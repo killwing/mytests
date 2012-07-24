9c11ba6fc875df1d6af808c13ea7864f8b242084
kelvin <kowochen@gmail.com>
2012-07-24 18:23:55 +0800

    use libc++

--- cpptest/main.cpp
+++ cpptest/main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 int main() {
-    auto a = 10;
-    cout << a << endl;
+    std::vector<int> v;
+    v.push_back(10);
+    v.push_back(12);
+    v.push_back(11);
+    auto it = std::find_if(v.cbegin(), v.cend(), [](int i) { return i == 11; });
+    cout << *it << endl;
     return 0;
 }
